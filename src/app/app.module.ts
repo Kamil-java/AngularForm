3c6dc461a2f432c38e2cc1188ef93c888e16c3ae
Kamil2001J <kamil.slowik@vp.pl>
2020-09-20 19:31:06 +0200

    Add all Project to check

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {FormComponent} from './Form/form/form.component';
+import {BudgetDetailComponent} from './Budget Detail/budget-detail/budget-detail.component';
+import {BudgetListComponent} from './Budget List/budget-list/budget-list.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    BudgetDetailComponent,
+    BudgetListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
